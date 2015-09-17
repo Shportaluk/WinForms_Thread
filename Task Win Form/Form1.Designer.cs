5cc68741f1580dddbe5ba6bf10552280303377cc
Shportaluk <andreyshportaluk@gmail.com>
2015-09-17 23:51:48 +0300

    Add proj

---
+++ Task Win Form/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Task_Win_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.col = new System.Windows.Forms.TextBox();
+            this.row = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(106, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Col:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(106, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Row:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // col
+            // 
+            this.col.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.col.Location = new System.Drawing.Point(169, 57);
+            this.col.Name = "col";
+            this.col.Size = new System.Drawing.Size(100, 26);
+            this.col.TabIndex = 2;
+            this.col.Text = "1";
+            this.col.TextChanged += new System.EventHandler(this.quantity_process_TextChanged);
+            // 
+            // row
+            // 
+            this.row.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.row.Location = new System.Drawing.Point(169, 19);
+            this.row.Name = "row";
+            this.row.Size = new System.Drawing.Size(100, 26);
+            this.row.TabIndex = 3;
+            this.row.Text = "100";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Start.Location = new System.Drawing.Point(313, 22);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 59);
+            this.btn_Start.TabIndex = 4;
+            this.btn_Start.Text = "Go";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Location = new System.Drawing.Point(38, 100);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(414, 316);
+            this.listView.TabIndex = 7;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(185, 377);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 447);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.row);
+            this.Controls.Add(this.col);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(510, 1024);
+            this.MinimumSize = new System.Drawing.Size(510, 145);
+            this.Name = "Form1";
+            this.Text = "Task";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox col;
+        private System.Windows.Forms.TextBox row;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Task Win Form/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_Win_Form
+{
+    public partial class Form1 : Form
+    {
+        public int count = 0;
+        public byte[][] b { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quantity_process_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void Sum(int id)
+        {
+            int sum = 0;
+            for(int i = 0; i < b[id].Length; i++)
+            {
+                sum += b[id][i];
+                SetValue(id);
+            }
+        }
+
+        public void For_col(object obj )
+        {
+            int id = (int)obj;
+            
+            for ( int i = 0; i < b.Length; i++ )
+            {
+                Sum(i);
+            }
+        }
+
+        public void SetValue( int id )
+        {
+            this.BeginInvoke((Action)(() =>
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    ProgressBar p = (listView.Controls[String.Format("progress_bar_{0}", id)] as ProgressBar);
+                    if (p.Value != p.Maximum)
+                    { p.Value = p.Value + 1; }
+                }
+            }));
+        }
+
+        //----
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            int _row;
+            int _col;
+
+            Int32.TryParse(col.Text, out _col);
+            Int32.TryParse(row.Text, out _row);
+
+            b = new byte[_row][];
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = new byte[_col];
+                for (int j = 0; j < b[i].Length; j++)
+                {
+                    b[i][j] = 1;
+                }
+            }
+            
+
+            for (int i = 0; i < _row; i++)
+            {
+                ProgressBar progress_bar = new ProgressBar();
+                progress_bar.Size = new System.Drawing.Size(420, 23);
+                progress_bar.Maximum = _col;
+                progress_bar.Location = new System.Drawing.Point(0, i * 25);
+                progress_bar.Name = String.Format("progress_bar_{0}", i);
+
+                count++;
+
+                listView.Controls.Add(progress_bar);
+            }
+            for (int i = 0; i < _row; i++)
+            {
+                Thread t = new Thread(For_col);
+                t.Start(i);
+            }
+
+        }
+    }
+}
